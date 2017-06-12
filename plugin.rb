35b6610a3cd0f17e0687ca824bb5ba63c181fd25
bloominglily <i@loli.co>
2017-06-12 16:17:16 +0800

    Create plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -110,6 +110,7 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
       if sso_record.external_username != result.username
         update_username(user, result.username)
         sso_record.external_username = result.username
+        sso_record.avatar_url = user_details[:avatar]
         sso_record.save!
       end
     else
